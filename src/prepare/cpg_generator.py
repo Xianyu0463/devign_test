9cbbed2d2877715b8db36abcb40963e66ae21fc7
sqZ <z5539614@ad.unsw.edu.au>
2026-06-24 16:47:24 +1000

    fix

--- src/prepare/cpg_generator.py
+++ src/prepare/cpg_generator.py
@@ -6,8 +6,6 @@ import os
 import time
 from .cpg_client_wrapper import CPGClientWrapper
 #from ..data import datamanager as data
-
-
 def funcs_to_graphs(funcs_path):
     client = CPGClientWrapper()
     # query the cpg for the dataset
@@ -16,78 +14,51 @@ def funcs_to_graphs(funcs_path):
     # removes unnecessary namespace for object references
     graphs_string = re.sub(r"io\.shiftleft\.codepropertygraph\.generated\.", '', graphs_string)
     graphs_json = json.loads(graphs_string)
-
     return graphs_json["functions"]
-
-
 def graph_indexing(graph):
     idx = int(graph["file"].split(".c")[0].split("/")[-1])
     del graph["file"]
     return idx, {"functions": [graph]}
-
-
 def joern_parse(joern_path, input_path, output_path, file_name):
     out_file = file_name + ".bin"
-    joern_parse_call = subprocess.run(["./" + joern_path + "joern-parse", input_path, "--output", output_path + out_file],
+    joern_parse_call = subprocess.run(["./" + joern_path + "joern-parse", input_path, "--out", output_path + out_file],
                                       stdout=subprocess.PIPE, text=True, check=True)
     print(str(joern_parse_call))
     return out_file
-
-
 def joern_create(joern_path, in_path, out_path, cpg_files):
-    env = os.environ.copy()
-    env["TERM"] = "dumb"
-    env["JAVA_OPTS"] = "-Djline.terminal=jline.UnsupportedTerminal"
-    joern_process = subprocess.Popen(
-        ["./" + joern_path + "joern", "--nocolors"],
-        stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-        env=env
-    )
     json_files = []
     for cpg_file in cpg_files:
         json_file_name = f"{cpg_file.split('.')[0]}.json"
         json_files.append(json_file_name)
-
-        print(in_path+cpg_file)
-        if os.path.exists(in_path+cpg_file):
+        print(in_path + cpg_file)
+        if os.path.exists(in_path + cpg_file):
             json_out = f"{os.path.abspath(out_path)}/{json_file_name}"
-            import_cpg_cmd = f"importCpg(\"{os.path.abspath(in_path)}/{cpg_file}\")\r".encode()
+            abs_cpg = f"{os.path.abspath(in_path)}/{cpg_file}"
             script_path = f"{os.path.dirname(os.path.abspath(joern_path))}/graph-for-funcs.sc"
-            run_script_cmd = f"cpg.runScript(\"{script_path}\").toString() |> \"{json_out}\"\r".encode()
-            def wait_for_prompt(proc, timeout=300):
-                import select
-                output = b""
-                while True:
-                    ready = select.select([proc.stdout], [], [], timeout)
-                    if ready[0]:
-                        chunk = proc.stdout.read(1)
-                        output += chunk
-                        if output.endswith(b"joern> "):
-                            return output.decode(errors="ignore")
-                    else:
-                        return output.decode(errors="ignore")
-            wait_for_prompt(joern_process)
-            joern_process.stdin.write(import_cpg_cmd)
-            joern_process.stdin.flush()
-            wait_for_prompt(joern_process)
-            joern_process.stdin.write(run_script_cmd)
-            joern_process.stdin.flush()
-            wait_for_prompt(joern_process, timeout=600)
-            joern_process.stdin.write("delete\r".encode())
-            joern_process.stdin.flush()
-            wait_for_prompt(joern_process)
-    try:
-        outs, errs = joern_process.communicate(timeout=7200)
-    except subprocess.TimeoutExpired:
-        joern_process.kill()
-        outs, errs = joern_process.communicate()
-    if outs is not None:
-        print(f"Outs: {outs.decode()}")
-    if errs is not None:
-        print(f"Errs: {errs.decode()}")
+            commands = (
+                f'importCpg("{abs_cpg}")\n'
+                f'cpg.runScript("{script_path}").toString() |> "{json_out}"\n'
+                f'delete\n'
+                f'exit\n'
+            )
+            env = os.environ.copy()
+            env["TERM"] = "dumb"
+            env["JAVA_OPTS"] = "-Djline.terminal=jline.UnsupportedTerminal"
+            try:
+                result = subprocess.run(
+                    ["./" + joern_path + "joern", "--nocolors"],
+                    input=commands.encode(),
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    env=env,
+                    timeout=600
+                )
+                print(f"Done: {json_file_name}")
+                if result.stdout:
+                    print(result.stdout.decode(errors="ignore")[:200])
+            except subprocess.TimeoutExpired:
+                print(f"Timeout: {cpg_file}")
     return json_files
-
-
 def json_process(in_path, json_file):
     if os.path.exists(in_path+json_file):
         with open(in_path+json_file) as jf:
@@ -97,7 +68,6 @@ def json_process(in_path, json_file):
             container = [graph_indexing(graph) for graph in cpg_json["functions"] if graph["file"] != "N/A"]
             return container
     return None
-
 '''
 def generate(dataset, funcs_path):
     dataset_size = len(dataset)
@@ -107,10 +77,8 @@ def generate(dataset, funcs_path):
     container = [graph_indexing(graph) for graph in graphs["functions"] if graph["file"] != "N/A"]
     graph_dataset = data.create_with_index(container, ["Index", "cpg"])
     print(f"Dataset processed.")
-
     return data.inner_join_by_index(dataset, graph_dataset)
 '''
-
 # client = CPGClientWrapper()
 # client.create_cpg("../../data/joern/")
 # joern_parse("../../joern/joern-cli/", "../../data/joern/", "../../joern/joern-cli/", "gen_test")
